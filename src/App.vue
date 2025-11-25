9c37c8d4972c0f4c939c1c87e1dce24b44f92785
nozredge <nozredge@proton.me>
2025-11-25 13:42:50 -0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <section class="section">
+    <div class="container">
+        <h1 class="title has-text-centered has-text-primary" style="font-size: 3rem; font-weight: 800;">
+            Hello KanbApp!
+        </h1>
+        <p class="subtitle has-text-centered has-text-grey mb-6">
+            A subtitle
+        </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
